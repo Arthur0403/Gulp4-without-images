4bab9be15a52a1207bdfaa2140976503c36bea9b
Arthur <alobanov1989@yandex.ru>
2019-02-12 23:59:10 +0300

    Started work

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 module.exports = function(){
     $.gulp.task('watch', function () {
-        $.gulp.watch('src/*.html', $.gulp.series('html'));
         $.gulp.watch('src/static/scss/**/*.scss', $.gulp.series('scss'));
         $.gulp.watch('src/static/js/**/*.js', $.gulp.series('scripts'));
+        $.gulp.watch('src/*.html', $.gulp.series('html'));
     });
 }
