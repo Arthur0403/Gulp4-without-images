65fd0a4591a694d82c5dc76c1c82beb237ed3147
Arthur <alobanov1989@yandex.ru>
2019-02-13 19:43:06 +0300

    Add shape blocks and css

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,28 @@
-$(document).ready(function () {
-    $('.slider').slick();
-});
+'use strict';
+
+// let arr = [];
+// while(true){
+//     let num = prompt('Введите число','');
+//     if(num == null || num == '' || isNaN(num)){
+//         break
+//     }
+
+//     arr.push(+num);
+
+//     var sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//       }
+// };
+
+    function find(array, value){
+    if(array.indexOf(value)){
+        // console.log(arr[i]);
+        return array.indexOf(value);
+    }
+    }
+
+    let arr = ['test', 2, 1.5, false];
+    console.log(find(arr, false));
+// console.log(arr);
+
