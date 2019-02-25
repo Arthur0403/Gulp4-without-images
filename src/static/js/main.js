a5f0b985636da716cbd20010bb4c2201077a16a4
Arthur <alobanov1989@yandex.ru>
2019-02-25 09:54:44 +0300

    25.02

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -26,3 +26,42 @@
     console.log(find(arr, false));
 // console.log(arr);
 
+function onScroll(){
+    let header = document.querySelector('.header');
+    let searchBlock = document.querySelector('.header-middle-wrapper');
+    let headerBottom = document.querySelector('.header-bottom');
+    let shapeItem = document.querySelectorAll('.shape-item');
+
+    window.addEventListener("scroll", callbackFunc);
+
+    function callbackFunc(){
+        let changeY = window.pageYOffset;
+        if(changeY > 150) {
+            header.classList.add('scroll');
+            searchBlock.classList.add('resize');
+            headerBottom.classList.add('header-bottom-resize');
+        } else {
+            header.classList.remove('scroll');
+            searchBlock.classList.remove('resize');
+            headerBottom.classList.remove('header-bottom-resize')
+        }
+
+        if(changeY > 900) {
+            for(let i = 0; i < shapeItem.length; i++){
+                shapeItem[i].classList.add('shape-item-visible');
+            }
+
+        } 
+        // else {
+        //     for(let i = 0; i < shapeItem.length; i++){
+        //         shapeItem[i].classList.remove('shape-item-visible');
+        //     }
+        // }
+    }
+}
+
+window.onload = function() {
+    onScroll();
+}
+
+
